627f83e5432bd6532232ed42697dabca81322304
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-03-25 17:05:31 +0100

    Remove tracking of rendered HTML files.

---
+++ R/data.R
@@ -0,0 +1,112 @@
+#' Anscombe's Quartet of ‘Identical’ Simple Linear Regressions
+#'
+#' Four \emph{x-y} datasets which have the same traditional statistical
+#' properties (mean, variance, correlation, regression line, etc.), yet are
+#' quite different.
+#'
+#' @format A \code{\link[tibble]{tibble}} with 11 observations of 8 variables:
+#' \describe{
+#'   \item{x1}{Vector of integers between 4 and 14.}
+#'   \item{x2}{Same vector as \code{x1}.}
+#'   \item{x3}{Same vector as \code{x1}.}
+#'   \item{x4}{Vector containing 8's and 19's.}
+#'   \item{y1}{Numbers between 3 and 12.5 with mean 7.5 and standard deviation
+#'   2.03 to be analysed together with x1.}
+#'   \item{y2}{Numbers between 3 and 12.5 with mean 7.5 and standard deviation
+#'   2.03 to be analysed together with x2.}
+#'   \item{y3}{Numbers between 3 and 12.5 with mean 7.5 and standard deviation
+#'   2.03 to be analysed together with x3.}
+#'   \item{y4}{Numbers between 3 and 12.5 with mean 7.5 and standard deviation
+#'   2.03 to be analysed together with x4.}
+#' }
+#'
+#' @source Tufte, Edward R. (1989). The Visual Display of Quantitative
+#'   Information, 13–14. Graphics Press.
+#'
+#' @references Anscombe, Francis J. (1973). Graphs in statistical analysis. The
+#'   American Statistician, 27, 17–21. doi: \url{10.2307/2682899}.
+"anscombe"
+
+#' Brain data set
+#'
+#' A data set containing the body weight and brain weight of 62 species.
+#'
+#' @format A tibble with 62 rows and 2 variables:
+#' \describe{
+#'   \item{body_weight}{body weight, in kg.}
+#'   \item{brain_weight}{brain weight, in kg.}
+#' }
+#' @source ???
+"brain"
+
+#' Speed and Stopping Distances of Cars
+#'
+#' The data give the speed of cars and the distances taken to stop. Note that
+#' the data were recorded in the 1920s.
+#'
+#' @format A tibble with 50 observations of 2 variables:
+#' \describe{
+#'   \item{speed}{Speed, in mph.}
+#'   \item{dist}{Stopping distance, in ft.}
+#' }
+#'
+#' @source Ezekiel, M. (1930) Methods of Correlation Analysis. Wiley.
+#'
+#' @references McNeil, D. R. (1977) Interactive Data Analysis. Wiley.
+"cars"
+
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"cement"
+
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"job"
+
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"mussels"
+
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"production"
